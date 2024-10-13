be6895dc012117a2746e5548124530a136dbc401
V-Gulev <velizargulev@abv.bg>
2024-10-13 19:00:01 +0300

    Exercise 5

--- Main.java
+++ Main.java
@@ -4,9 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println(-5 + 8 * 6);
-        System.out.println((55 + 9) % 9);
-        System.out.println(20 + -3 * 5 / 8);
-        System.out.println(5 + 15 / 3 * 2 - 8 % 3);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int result = a * b;
+        System.out.printf("%d x %d = %d", a, b, result);
     }
 }
