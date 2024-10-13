f5df71319c7d2ae4f480843a53314e60454f5139
V-Gulev <velizargulev@abv.bg>
2024-10-13 18:56:35 +0300

    Exercise 3

--- Main.java
+++ Main.java
@@ -6,6 +6,6 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         int a = scanner.nextInt();
         int b = scanner.nextInt();
-        System.out.println(a + b);
+        System.out.println(a / b);
     }
 }
