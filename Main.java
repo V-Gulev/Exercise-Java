a67f2f8f93a7585fb09daa4286ae071eff3c8968
V-Gulev <velizargulev@abv.bg>
2024-10-13 18:54:35 +0300

    Exercise 1

--- Main.java
+++ Main.java
@@ -4,63 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        int sizeOfField = Integer.parseInt(scanner.nextLine());
-        int[] ladyBugArray = new int[sizeOfField];
-
-        String[] ladyBugsPosition = scanner.nextLine().split(" ");
-        for (String pos : ladyBugsPosition) {
-            int index = Integer.parseInt(pos);
-            if (index >= 0 && index < sizeOfField) {
-                ladyBugArray[index] = 1;
-            }
-        }
-
-        while (true) {
-            String input = scanner.nextLine();
-
-            if (input.equals("end")) {
-
-                for (int i : ladyBugArray) {
-                    System.out.print(i + " ");
-                }
-                break;
-            } else {
-                String[] command = input.split(" ");
-                int ladyBugIndex = Integer.parseInt(command[0]);
-                String direction = command[1];
-                int flyLength = Integer.parseInt(command[2]);
-
-                if (flyLength < 0) {
-                    flyLength = Math.abs(flyLength);
-                    if (direction.equals("right")) {
-                        direction = "left";
-                    } else direction = "right";
-                }
-
-                if (ladyBugIndex < 0 || ladyBugIndex >= sizeOfField || ladyBugArray[ladyBugIndex] == 0) {
-                    continue;
-                }
-
-                ladyBugArray[ladyBugIndex] = 0;
-                while (true) {
-                    if (direction.equals("right")) {
-                        ladyBugIndex += flyLength;
-                    } else if (direction.equals("left")) {
-                        ladyBugIndex -= flyLength;
-                    }
-
-                    if (ladyBugIndex < 0 || ladyBugIndex >= sizeOfField) {
-                        break;
-                    }
-
-                    if (ladyBugArray[ladyBugIndex] == 0) {
-                        ladyBugArray[ladyBugIndex] = 1;
-                        break;
-                    }
-
-                }
-            }
-        }
+        System.out.println("Enter your name:");
+        String name = scanner.nextLine();
+        System.out.printf("Hello%n%s",name);
     }
 }
