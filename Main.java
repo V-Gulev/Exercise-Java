c9e9d3091409ace8072925aceaf67187c093a1ab
V-Gulev <velizargulev@abv.bg>
2024-10-13 19:26:31 +0300

    Exercise 16

--- Main.java
+++ Main.java
@@ -4,12 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
-        int temp = a;
-        System.out.println("a = " + a + " and b = " + b);
-        a = b;
-        b = temp;
-        System.out.println("a = " + a + " and b = " + b);
+        System.out.println(" +\"\"\"\"\"+ ");
+        System.out.println("[| o o |]");
+        System.out.println(" |  ^  | ");
+        System.out.println(" | '-' | ");
+        System.out.println(" +-----+ ");
     }
 }
