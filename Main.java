94d12d225ed1a7c319fcce1943b191b17cba4658
V-Gulev <velizargulev@abv.bg>
2024-10-13 19:23:29 +0300

    Exercise 14

--- Main.java
+++ Main.java
@@ -4,14 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        double width = Double.parseDouble(scanner.nextLine());
-        double height = Double.parseDouble(scanner.nextLine());
+        String a = "* * * * * * ==================================\n * * * * *  ==================================";
+        String b = "==============================================";
 
-        double perimeter = 2 * (height + width);
-        double area = width * height;
+        for (int i = 0; i < 4; i++) {
+            System.out.println(a);
+        }
+        System.out.println("* * * * * * ==================================");
 
-
-        System.out.printf("Perimeter is 2*(%.1f + %.1f) = %.2f \n", height, width, perimeter);
-        System.out.printf("Area is %.1f * %.1f = %.2f \n", width, height, area);
+        for (int i = 0; i < 6; i++) {
+            System.out.println(b);
+        }
     }
 }
