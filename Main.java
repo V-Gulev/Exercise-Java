0fa01e449fc9bb27aa37f631cbb1b8fd087b06b6
V-Gulev <velizargulev@abv.bg>
2024-10-13 19:38:20 +0300

    Exercise 19

--- Main.java
+++ Main.java
@@ -4,16 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Input first binary number: ");
-        String binary1 = sc.nextLine();
-        System.out.print("Input second binary number: ");
-        String binary2 = sc.nextLine();
-        int number1 = Integer.parseInt(binary1, 2);
-        int number2 = Integer.parseInt(binary2, 2);
-        int result = number1 * number2;
+        System.out.print("Input a Decimal Number: ");
+        int decimalNumber = sc.nextInt();
+        String binaryNumber = Integer.toBinaryString(decimalNumber);
 
-        String binarySum = Integer.toBinaryString(result);
-
-        System.out.println("Sum of two binary numbers: " + binarySum);
+        System.out.println("Binary number is: " + binaryNumber);
     }
 }
