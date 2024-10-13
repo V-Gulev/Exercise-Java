efa5b37e962eee158f2353e12784dd43a7b4ff49
V-Gulev <velizargulev@abv.bg>
2024-10-13 19:12:48 +0300

    Exercise 10

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5));
+        double result = 4.0 * (1 - (1.0 / 3) + (1.0 / 5) - (1.0 / 7) + (1.0 / 9) - (1.0 / 11));
+        System.out.println(result);
     }
 }
