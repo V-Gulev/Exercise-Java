9a8faf37f82fc7eaa0737504563ef91921a0e457
V-Gulev <velizargulev@abv.bg>
2024-10-13 19:34:38 +0300

    Exercise 17

--- Main.java
+++ Main.java
@@ -3,11 +3,17 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println(" +\"\"\"\"\"+ ");
-        System.out.println("[| o o |]");
-        System.out.println(" |  ^  | ");
-        System.out.println(" | '-' | ");
-        System.out.println(" +-----+ ");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input first binary number: ");
+        String binary1 = sc.nextLine();
+        System.out.print("Input second binary number: ");
+        String binary2 = sc.nextLine();
+        int number1 = Integer.parseInt(binary1, 2);
+        int number2 = Integer.parseInt(binary2, 2);
+        int sum = number1 + number2;
+
+        String binarySum = Integer.toBinaryString(sum);
+
+        System.out.println("Sum of two binary numbers: " + binarySum);
     }
 }
