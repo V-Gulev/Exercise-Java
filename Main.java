6c5e1b45a11529805c196540bce2b6e5a2ee3e8f
V-Gulev <velizargulev@abv.bg>
2024-10-13 19:12:05 +0300

    Exercise 9

--- Main.java
+++ Main.java
@@ -4,9 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("    J      a    V     V    a  ");
-        System.out.println("    J     a a    V   V    a a   ");
-        System.out.println("J   J    aaaaa    V V    aaaaa   ");
-        System.out.println(" JJ     a     a    V    a     a");
+        System.out.println((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5));
     }
 }
