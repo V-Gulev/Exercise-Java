4c92e2dcae3321597a80cfd6af605f625ac1a83b
V-Gulev <velizargulev@abv.bg>
2024-10-13 19:06:41 +0300

    Exercise 7

--- Main.java
+++ Main.java
@@ -5,12 +5,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int a = scanner.nextInt();
-        int b = scanner.nextInt();
-        System.out.printf("%d x %d = %d%n", a, b, a * b);
-        System.out.printf("%d - %d = %d%n", a, b, a - b);
-        System.out.printf("%d + %d = %d%n", a, b, a + b);
-        System.out.printf("%d / %d = %d%n", a, b, a / b);
-        System.out.printf("%d mod %d = %d%n", a, b, a % b);
 
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(a + " x " + i + " = " + (a * i));
+        }
     }
 }
