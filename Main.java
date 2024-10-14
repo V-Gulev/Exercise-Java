648bee8721a4a6083c45f7505e3b3d5f5030ed67
V-Gulev <velizargulev@abv.bg>
2024-10-14 14:48:26 +0300

    Exercise 37

--- Main.java
+++ Main.java
@@ -4,23 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Input the latitude of coordinate 1: ");
-        double x1 = sc.nextDouble();
-        System.out.print("Input the longitude of coordinate 1: ");
-        double y1 = sc.nextDouble();
-        System.out.print("Input the latitude of coordinate 2: ");
-        double x2 = sc.nextDouble();
-        System.out.print("Input the longitude of coordinate 2: ");
-        double y2 = sc.nextDouble();
-
-        double lat1 = Math.toRadians(x1);
-        double lon1 = Math.toRadians(y1);
-        double lat2 = Math.toRadians(x2);
-        double lon2 = Math.toRadians(y2);
-        double earthRadius = 6371.01;
-        double d = earthRadius * Math.acos(Math.sin(lat1) * Math.sin(lat2) + Math.cos(lat1) * Math.cos(lat2) * Math.cos(lon1 - lon2));
-        System.out.println("The distance between those points is: " + d + " km");
-
-
+        System.out.print("Input a string: ");
+        char[] input = sc.nextLine().toCharArray();
+        System.out.print("Reverse string: ");
+        for (int i = input.length - 1; i >= 0; i--) {
+            System.out.print(input[i]);
+        }
     }
 }
