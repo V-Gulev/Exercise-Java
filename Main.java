fe8863f55a69350e87c5c3859b34cd32330dae13
V-Gulev <velizargulev@abv.bg>
2024-10-14 14:20:24 +0300

    Exercise 32

--- Main.java
+++ Main.java
@@ -4,11 +4,23 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Java Version: " + System.getProperty("java.version"));
-        System.out.println("Java Runtime Version: " + System.getProperty("java.runtime.version"));
-        System.out.println("Java Home: " + System.getProperty("java.home"));
-        System.out.println("Java Vendor: " + System.getProperty("java.vendor"));
-        System.out.println("Java Vendor URL: " + System.getProperty("java.vendor.url"));
-        System.out.println("Java Class Path: " + System.getProperty("java.class.path"));
+        System.out.println("Insert your first number:");
+        int a = sc.nextInt();
+        System.out.println("Insert your second number:");
+        int b = sc.nextInt();
+        if (a == b)
+            System.out.printf("%d == %d\n", a, b);
+        if (a != b)
+            System.out.printf("%d != %d\n", a, b);
+        if (a < b)
+            System.out.printf("%d < %d\n", a, b);
+        if (a > b)
+            System.out.printf("%d > %d\n", a, b);
+        if (a <= b)
+            System.out.printf("%d <= %d\n", a, b);
+        if (a >= b)
+            System.out.printf("%d >= %d\n", a, b);
+
+
     }
 }
