a41da78664176e83eabdbb08620a158f3799bc6f
V-Gulev <velizargulev@abv.bg>
2024-10-14 14:07:36 +0300

    Exercise 25

--- Main.java
+++ Main.java
@@ -4,10 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Input a binary number: ");
-        String binaryNumber = sc.nextLine();
-        int decimalNumber = Integer.parseInt(binaryNumber, 2);
-        String hexNumber = Integer.toOctalString(decimalNumber);
-        System.out.println("Octal number: " + hexNumber);
+        System.out.print("Input a octal number: ");
+        String octalNumber = sc.nextLine();
+        int decimalNumber = Integer.parseInt(octalNumber, 8);
+        System.out.println("Octal number: " + decimalNumber);
     }
 }
