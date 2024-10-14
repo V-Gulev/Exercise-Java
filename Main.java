a17dabf79c00e92a5a84727139664d811352e490
V-Gulev <velizargulev@abv.bg>
2024-10-14 14:22:53 +0300

    Exercise 33

--- Main.java
+++ Main.java
@@ -4,22 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Insert your first number:");
+        System.out.print("Insert your first number: ");
         int a = sc.nextInt();
-        System.out.println("Insert your second number:");
-        int b = sc.nextInt();
-        if (a == b)
-            System.out.printf("%d == %d\n", a, b);
-        if (a != b)
-            System.out.printf("%d != %d\n", a, b);
-        if (a < b)
-            System.out.printf("%d < %d\n", a, b);
-        if (a > b)
-            System.out.printf("%d > %d\n", a, b);
-        if (a <= b)
-            System.out.printf("%d <= %d\n", a, b);
-        if (a >= b)
-            System.out.printf("%d >= %d\n", a, b);
+        int sumOfDigits = 0;
+
+        while (a > 0) {
+            int lastDigit = a % 10;
+            sumOfDigits += lastDigit;
+            a /= 10;
+        }
+        System.out.println("The sum of the digits is: " + sumOfDigits);
 
 
     }
