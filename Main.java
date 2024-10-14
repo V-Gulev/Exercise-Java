e144256a4c24b629145c1daf77c9523329bdfbaf
V-Gulev <velizargulev@abv.bg>
2024-10-14 14:12:14 +0300

    Exercise 28

--- Main.java
+++ Main.java
@@ -5,9 +5,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Input a octal number: ");
-        String octalNumber = sc.nextLine();
-        int decimalNumber = Integer.parseInt(octalNumber, 8);
-        String hexNumber = Integer.toHexString(decimalNumber);
-        System.out.println("Hexadecimal number: " + hexNumber);
+        String hexNumber = sc.nextLine();
+        int decimalNumber = Integer.parseInt(hexNumber, 16);
+
+        System.out.println("Decimal number: " + decimalNumber);
     }
 }
