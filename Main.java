ee930569377c6c171612e822d6092001600d089d
V-Gulev <velizargulev@abv.bg>
2024-10-14 15:18:02 +0300

    Exercise 41

--- Main.java
+++ Main.java
@@ -1,15 +1,10 @@
-import java.nio.charset.Charset;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("List of available character sets: ");
-
-        for (String str : Charset.availableCharsets().keySet()) {
-            System.out.println(str);
-        }
+        char ch = sc.nextLine().charAt(0);
+        System.out.println((int) ch);
     }
 }
