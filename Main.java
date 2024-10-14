ed676cc144bbf26dff06c0e2f5977a16b2ddd547
V-Gulev <velizargulev@abv.bg>
2024-10-14 14:57:42 +0300

    Exercise 38

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -5,10 +6,25 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Input a string: ");
-        char[] input = sc.nextLine().toCharArray();
-        System.out.print("Reverse string: ");
-        for (int i = input.length - 1; i >= 0; i--) {
-            System.out.print(input[i]);
+        String input = sc.nextLine();
+        char[] array = input.toCharArray();
+        int numCounter = 0;
+        int letterCounter = 0;
+        int spaceCounter = 0;
+        int otherCounter = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (Character.isAlphabetic(array[i])) {
+                letterCounter++;
+            } else if (Character.isDigit(array[i])) {
+                numCounter++;
+            } else if (Character.isSpaceChar(array[i])) {
+                spaceCounter++;
+            } else otherCounter++;
         }
+        System.out.println("The string is: " + input);
+        System.out.println("letter: " + letterCounter);
+        System.out.println("space: " + spaceCounter);
+        System.out.println("number: " + numCounter);
+        System.out.println("other: " + otherCounter);
     }
 }
