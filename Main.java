4da43fbb17c3e89dbc458cd330b46e035e72d9bd
V-Gulev <velizargulev@abv.bg>
2024-10-14 14:31:20 +0300

    Exercise 35

--- Main.java
+++ Main.java
@@ -4,9 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.print("Input the number of sides on the polygon: ");
+        int n = sc.nextInt();
         System.out.print("Input the length of a side  of the hexagon: ");
         int s = sc.nextInt();
-        double area = (6 * Math.pow(s, 2)) / (4 * Math.tan(Math.PI / 6));
+        double area = (n * Math.pow(s, 2)) / (4 * Math.tan(Math.PI / n));
         System.out.println("The area of the hexagon is: " + area);
 
 
