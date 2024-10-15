9870b054da49b8a57816e53e3f3f58f6109b7a87
V-Gulev <velizargulev@abv.bg>
2024-10-15 18:33:40 +0300

    Counter Strike

--- Main.java
+++ Main.java
@@ -1,24 +1,35 @@
-import java.io.Console;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Console cons;
+        int initialEnergy = Integer.parseInt(sc.nextLine());
+        int countWonBattles = 0;
+        String input = sc.nextLine();
+        boolean noEnergyLeft = false;
+        while (!input.equals("End of battle")) {
+            int distance = Integer.parseInt(input);
 
-        if ((cons = System.console()) != null) {
-            char[] password = null;
-            try {
-                password = cons.readPassword("Input your Password:");
-                System.out.println("Your password was: " + new String(password));
-            } finally {
-                if (password != null) {
-                    java.util.Arrays.fill(password, ' ');
-                }
+            if (initialEnergy < distance) {
+                noEnergyLeft = true;
+                break;
+            } else {
+                initialEnergy -= distance;
+                countWonBattles++;
             }
-        } else {
-            throw new RuntimeException("Can't get the password... No console");
+            if (countWonBattles % 3 == 0) {
+                initialEnergy += countWonBattles;
+            }
+            input = sc.nextLine();
         }
+
+        if (noEnergyLeft) {
+            System.out.printf("Not enough energy! Game ends with %d won battles and %d energy", countWonBattles, initialEnergy);
+        } else {
+            System.out.printf("Won battles: %d. Energy left: %d", countWonBattles, initialEnergy);
+        }
+
     }
 }
