77ad1d88405b13ebdfe1faa8589dee5857f9ba94
V-Gulev <velizargulev@abv.bg>
2024-10-15 18:47:24 +0300

    Shopping List

--- Main.java
+++ Main.java
@@ -1,35 +1,48 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int initialEnergy = Integer.parseInt(sc.nextLine());
-        int countWonBattles = 0;
+        List<String> shoppingList = Arrays.stream(sc.nextLine().split("!")).collect(Collectors.toList());
         String input = sc.nextLine();
-        boolean noEnergyLeft = false;
-        while (!input.equals("End of battle")) {
-            int distance = Integer.parseInt(input);
+        while (!input.equals("Go Shopping!")) {
+            String[] commands = input.split(" ");
+            String action = commands[0];
+            String item = commands[1];
 
-            if (initialEnergy < distance) {
-                noEnergyLeft = true;
-                break;
-            } else {
-                initialEnergy -= distance;
-                countWonBattles++;
-            }
-            if (countWonBattles % 3 == 0) {
-                initialEnergy += countWonBattles;
+            switch (action) {
+                case "Urgent":
+                    if (!shoppingList.contains(item)) {
+                        shoppingList.addFirst(item);
+                    }
+                    break;
+                case "Unnecessary":
+                    if (shoppingList.contains(item)) {
+                        shoppingList.remove(item);
+                    }
+                    break;
+                case "Correct":
+                    String item2 = commands[2];
+                    if (shoppingList.contains(item)) {
+                        shoppingList.set(shoppingList.indexOf(item), item2);
+                    }
+                    break;
+                case "Rearrange":
+                    if (shoppingList.contains(item)) {
+                        shoppingList.remove(item);
+                        shoppingList.add(item);
+                    }
+                    break;
+                default:
+                    break;
+
             }
             input = sc.nextLine();
         }
-
-        if (noEnergyLeft) {
-            System.out.printf("Not enough energy! Game ends with %d won battles and %d energy", countWonBattles, initialEnergy);
-        } else {
-            System.out.printf("Won battles: %d. Energy left: %d", countWonBattles, initialEnergy);
-        }
-
+        System.out.println(shoppingList.toString().replaceAll("[\\[\\]]", ""));
     }
 }
