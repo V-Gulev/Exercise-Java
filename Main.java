cd00b1878f482b280ebad30b815ee754da66c779
V-Gulev <velizargulev@abv.bg>
2024-10-15 19:31:12 +0300

    Memory game

--- Main.java
+++ Main.java
@@ -7,42 +7,51 @@ import java.util.stream.Collectors;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        List<String> shoppingList = Arrays.stream(sc.nextLine().split("!")).collect(Collectors.toList());
+        List<String> memoryList = Arrays.stream(sc.nextLine().split(" ")).collect(Collectors.toList());
         String input = sc.nextLine();
-        while (!input.equals("Go Shopping!")) {
-            String[] commands = input.split(" ");
-            String action = commands[0];
-            String item = commands[1];
+        int moveCounter = 0;
+        boolean checkIfWon = false;
 
-            switch (action) {
-                case "Urgent":
-                    if (!shoppingList.contains(item)) {
-                        shoppingList.addFirst(item);
-                    }
-                    break;
-                case "Unnecessary":
-                    if (shoppingList.contains(item)) {
-                        shoppingList.remove(item);
-                    }
-                    break;
-                case "Correct":
-                    String item2 = commands[2];
-                    if (shoppingList.contains(item)) {
-                        shoppingList.set(shoppingList.indexOf(item), item2);
-                    }
-                    break;
-                case "Rearrange":
-                    if (shoppingList.contains(item)) {
-                        shoppingList.remove(item);
-                        shoppingList.add(item);
-                    }
-                    break;
-                default:
-                    break;
+        while (!input.equals("end")) {
+            String[] array = input.split(" ");
+            int int1 = Integer.parseInt(array[0]);
+            int int2 = Integer.parseInt(array[1]);
+            moveCounter++;
 
+            if (int1 == int2 || int1 < 0 || int1 >= memoryList.size() || int2 < 0 || int2 >= memoryList.size()) {
+                System.out.println("Invalid input! Adding additional elements to the board");
+                String element = "-" + moveCounter + "a";
+                int index = memoryList.size() / 2;
+                memoryList.add(index, element);
+                memoryList.add(index, element);
+            } else {
+                if (memoryList.get(int1).equals(memoryList.get(int2))) {
+                    System.out.printf("Congrats! You have found matching elements - %s!\n", memoryList.get(int1));
+
+                    if (int1 > int2) {
+                        memoryList.remove(int1);
+                        memoryList.remove(int2);
+                    } else {
+                        memoryList.remove(int2);
+                        memoryList.remove(int1);
+                    }
+                    if (memoryList.isEmpty()) {
+                        System.out.printf("You have won in %d turns!\n", moveCounter);
+                        checkIfWon = true;
+                        break;
+                    }
+
+                } else {
+                    System.out.println("Try again!");
+                }
             }
+
             input = sc.nextLine();
         }
-        System.out.println(shoppingList.toString().replaceAll("[\\[\\]]", ""));
+
+        if (!checkIfWon) {
+            System.out.println("Sorry you lose :(");
+            System.out.println(memoryList.toString().replaceAll("[\\[\\],]", ""));
+        }
     }
 }
