03be709268e219cad967e30d88d86ac96b0bb486
V-Gulev <velizargulev@abv.bg>
2024-10-16 17:11:16 +0300

    Exercise 66

--- Main.java
+++ Main.java
@@ -4,12 +4,31 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Input the first number: ");
-        int num1 = sc.nextInt();
-        System.out.print("Input the second number: ");
-        int num2 = sc.nextInt();
-        int divided = num1 / num2;
-        int result = num1 - (divided * num2);
-        System.out.println(result);
+        int count = 0;
+        int sum = 0;
+        int number = 2;
+
+        while (count < 100) {
+            if (isPrime(number)) {
+                sum += number;
+                count++;
+            }
+            number++;
+        }
+
+        System.out.println("Sum of the first 100 prime numbers: " + sum);
+
+    }
+
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
