3ad9b4ce8bcd6a6779060b512741c182f0dcc6c8
V-Gulev <velizargulev@abv.bg>
2024-10-16 15:14:09 +0300

    Exercise 46

--- Main.java
+++ Main.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("/user/Developer/Java-Exercise : " + new File("exercise.txt").length() + " bytes");
-        System.out.println("/user/Developer/Java-Exercise : " + new File("exam.txt").length() + " bytes");
+        System.out.printf("Current Date time: %tc%n", System.currentTimeMillis());
     }
 }
