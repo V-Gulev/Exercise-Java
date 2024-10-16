55687a13152dd4b5677b48f3298c6161f5e4911c
V-Gulev <velizargulev@abv.bg>
2024-10-16 15:42:14 +0300

    Exercise 53

--- Main.java
+++ Main.java
@@ -9,8 +9,12 @@ public class Main {
         int num2 = Integer.parseInt(sc.nextLine());
         System.out.print("Input the third number:  ");
         int num3 = Integer.parseInt(sc.nextLine());
-        if (num1 + num2 == num3) {
-            System.out.println("true");
-        } else System.out.println("false");
+        System.out.print("Input a boolean value (true/false): ");
+        boolean xyz = Boolean.parseBoolean(sc.nextLine());
+        if (xyz && num3 > num2) {
+            System.out.println(true);
+        } else if (num3 > num2 && num2 > num1) {
+            System.out.println(true);
+        } else System.out.println(false);
     }
 }
