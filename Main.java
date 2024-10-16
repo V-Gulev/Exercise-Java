eae5a4f64b39bede3ea2b87519664f309993c586
V-Gulev <velizargulev@abv.bg>
2024-10-16 15:24:43 +0300

    Exercise 47

--- Main.java
+++ Main.java
@@ -1,9 +1,13 @@
-import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Scanner;
+import java.util.TimeZone;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.printf("Current Date time: %tc%n", System.currentTimeMillis());
+        SimpleDateFormat time = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss.SSS");
+        time.setCalendar(Calendar.getInstance(TimeZone.getTimeZone("GMT")));
+        System.out.println("Now: " + time.format(System.currentTimeMillis()));
     }
 }
