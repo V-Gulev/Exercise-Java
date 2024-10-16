4f40b1571e105343242d5335e70d5742f4d1da42
V-Gulev <velizargulev@abv.bg>
2024-10-16 16:35:52 +0300

    Exercise 58

--- Main.java
+++ Main.java
@@ -1,18 +1,20 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Input an integer: ");
-        int number = sc.nextInt();
-
-        int factorCount = 0;
-
-        for (int i = 1; i <= number; i++) {
-            if (number % i == 0) {
-                factorCount++;
-            }
+        System.out.print("Input a sentence: ");
+        String[] words = sc.nextLine().split(" ");
+        String capitalWord = "";
+        String result = "";
+        int index = 0;
+        for (String word : words) {
+            if (Character.isAlphabetic(word.charAt(0))) {
+                capitalWord = Character.toUpperCase(word.charAt(0)) + word.substring(1);
+            } else capitalWord = word;
+            result = result + capitalWord + " ";
         }
-        System.out.println(factorCount);
+        System.out.println(result.trim());
     }
 }
