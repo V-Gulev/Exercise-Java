b34f99c2418cc4051e18bab698b4e78c53adfc8d
V-Gulev <velizargulev@abv.bg>
2024-10-16 15:27:30 +0300

    Exercise 49

--- Main.java
+++ Main.java
@@ -3,8 +3,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        for (int i = 1; i < 100; i += 2) {
-            System.out.println(i);
-        }
+        int n = Integer.parseInt(sc.nextLine());
+        if (n % 2 == 0) {
+            System.out.println(1);
+        } else System.out.println(0);
     }
 }
