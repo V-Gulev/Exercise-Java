04677d876108b1788b2a65efb54f14cecb170ae1
V-Gulev <velizargulev@abv.bg>
2024-10-16 16:00:36 +0300

    Exercise 56

--- Main.java
+++ Main.java
@@ -3,12 +3,16 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Input the amount of seconds: ");
-        int input = Integer.parseInt(sc.nextLine());
-        int seconds = input % 60;
-        int hours = input / 60 / 60;
-        int minutes = input / 60 % 60;
-        System.out.println(hours + ":" + minutes + ":" + seconds);
+        int start = sc.nextInt();
+        int end = sc.nextInt();
+        int divider = sc.nextInt();
+        int counter = 0;
+        for (int i = start; i <= end; i++) {
+            if (i % divider == 0) {
+                counter++;
+            }
+        }
+        System.out.println(counter);
 
     }
 }
