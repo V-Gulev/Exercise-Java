fb33759429f22533f9fa829bbacc9a138971c952
V-Gulev <velizargulev@abv.bg>
2024-10-16 15:34:50 +0300

    Exercise 52

--- Main.java
+++ Main.java
@@ -3,8 +3,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Input a number(string): ");
-        String number = sc.nextLine();
-        System.out.println("The integer value is: " + Integer.parseInt(number));
+        System.out.print("Input the first number:  ");
+        int num1 = Integer.parseInt(sc.nextLine());
+        System.out.print("Input the second number:  ");
+        int num2 = Integer.parseInt(sc.nextLine());
+        System.out.print("Input the third number:  ");
+        int num3 = Integer.parseInt(sc.nextLine());
+        if (num1 + num2 == num3) {
+            System.out.println("true");
+        } else System.out.println("false");
     }
 }
