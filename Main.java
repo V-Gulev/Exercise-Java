e5e535fdf44cea62e979183c8eb33e10598d9fdf
V-Gulev <velizargulev@abv.bg>
2024-10-16 16:43:37 +0300

    Exercise 61

--- Main.java
+++ Main.java
@@ -4,9 +4,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Input a sentence: ");
-        String[] words = sc.nextLine().split(" ");
+        System.out.print("Input a word: ");
+        String word = sc.nextLine();
+        char[] ch = word.toCharArray();
 
-        System.out.println(words[words.length - 2]);
+        for (int i = ch.length - 1; i >= 0; i--) {
+            System.out.print(ch[i]);
+        }
+
     }
 }
