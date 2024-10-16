6f4af6b6c1c69d6fc2d0bd120e39dd8240ad5f99
V-Gulev <velizargulev@abv.bg>
2024-10-16 17:20:30 +0300

    Exercise 67

--- Main.java
+++ Main.java
@@ -1,34 +1,16 @@
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int count = 0;
-        int sum = 0;
-        int number = 2;
-
-        while (count < 100) {
-            if (isPrime(number)) {
-                sum += number;
-                count++;
-            }
-            number++;
-        }
-
-        System.out.println("Sum of the first 100 prime numbers: " + sum);
-
+        List<String> input = Arrays.stream(sc.nextLine().split(" ")).collect(Collectors.toList());
+        String wordToAdd = sc.nextLine();
+        int index = input.size() / 2;
+        input.add(index, wordToAdd);
+        System.out.println(input.toString().replaceAll("[\\[\\],]", ""));
     }
 
-    public static boolean isPrime(int num) {
-        if (num <= 1) {
-            return false;
-        }
-        for (int i = 2; i <= Math.sqrt(num); i++) {
-            if (num % i == 0) {
-                return false;
-            }
-        }
-        return true;
-    }
 }
