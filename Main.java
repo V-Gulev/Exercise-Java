6f431e5fc6219e3c34b8caee35034b915bc81b15
V-Gulev <velizargulev@abv.bg>
2024-10-16 17:56:11 +0300

    Exercise 73

--- Main.java
+++ Main.java
@@ -7,14 +7,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String word1 = sc.nextLine();
-        String str1 = "###";
-        if (word1.length() >= 3) {
-            str1 = word1.substring(0, 3);
-        } else if (word1.length() == 2) {
-            str1 = word1.substring(0, 2) + "#";
-        } else if (word1.length() == 1) {
-            str1 = word1.charAt(0) + "##";
-        }
+        String word2 = sc.nextLine();
+        String str1 = "";
+        str1 += (!word1.isEmpty()) ? word1.charAt(0) : "#";
+        str1 += (!word2.isEmpty()) ? word2.charAt(word2.length() - 1) : "#";
         System.out.println(str1);
     }
 }
