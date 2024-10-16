3a922819deea3bff3e164a9280e880b010302f2f
V-Gulev <velizargulev@abv.bg>
2024-10-16 15:55:43 +0300

    Exercise 55

--- Main.java
+++ Main.java
@@ -3,17 +3,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Input the first number: ");
-        int num1 = Integer.parseInt(sc.nextLine());
-        int lastDigit1 = num1 % 10;
-        System.out.print("Input the second number: ");
-        int num2 = Integer.parseInt(sc.nextLine());
-        int lastDigit2 = num2 % 10;
-        System.out.print("Input the third number: ");
-        int num3 = Integer.parseInt(sc.nextLine());
-        int lastDigit3 = num3 % 10;
+        System.out.print("Input the amount of seconds: ");
+        int input = Integer.parseInt(sc.nextLine());
+        int seconds = input % 60;
+        int hours = input / 60 / 60;
+        int minutes = input / 60 % 60;
+        System.out.println(hours + ":" + minutes + ":" + seconds);
 
-        boolean checkIfEqual = (lastDigit1 == lastDigit2) || (lastDigit1 == lastDigit3) || (lastDigit2 == lastDigit3);
-        System.out.println(checkIfEqual);
     }
 }
