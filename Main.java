d0d4c7515cad8b242df53f0c440e3fcc6b196460
V-Gulev <velizargulev@abv.bg>
2024-10-16 15:07:11 +0300

    Exercise 44

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("\nTwinkle, twinkle, little star, \n\tHow I wonder what you are! \n\t\tUp above the world so high, " +
-                "\n\t\tLike a diamond in the sky. \nTwinkle, twinkle, little star, \n\tHow I wonder what you are!\n\n");
+        int n = Integer.parseInt(sc.nextLine());
+        System.out.printf("%d + %d%d + %d%d%d", n, n, n, n, n, n);
     }
 }
