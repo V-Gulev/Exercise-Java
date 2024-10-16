47e4bfbe18919a912ea3cac4038f74af71013049
V-Gulev <velizargulev@abv.bg>
2024-10-16 15:25:50 +0300

    Exercise 48

--- Main.java
+++ Main.java
@@ -1,13 +1,10 @@
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Scanner;
-import java.util.TimeZone;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        SimpleDateFormat time = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss.SSS");
-        time.setCalendar(Calendar.getInstance(TimeZone.getTimeZone("GMT")));
-        System.out.println("Now: " + time.format(System.currentTimeMillis()));
+        for (int i = 1; i < 100; i += 2) {
+            System.out.println(i);
+        }
     }
 }
