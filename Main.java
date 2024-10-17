92d30509f9a177bd18bbb47a64ddd2ed953aefbf
V-Gulev <velizargulev@abv.bg>
2024-10-17 15:38:49 +0300

    Exercise 77

--- Main.java
+++ Main.java
@@ -6,7 +6,10 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         List<Integer> nums1 = Arrays.stream(sc.nextLine().split(" ")).map(Integer::parseInt).collect(Collectors.toList());
         List<Integer> nums2 = Arrays.stream(sc.nextLine().split(" ")).map(Integer::parseInt).collect(Collectors.toList());
-        System.out.println(nums1.getFirst().equals(nums2.getFirst()) || nums1.getLast().equals(nums2.getLast()));
+        int[] array = new int[2];
+        array[0] = nums1.getFirst();
+        array[1] = nums2.getLast();
+        System.out.println(Arrays.toString(array).replaceAll("[\\[\\],]", ""));
 
     }
 }
