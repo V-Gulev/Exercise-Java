01b839d909f8cdb628ff72db93520c681f5982f3
V-Gulev <velizargulev@abv.bg>
2024-10-17 15:36:12 +0300

    Exercise 76

--- Main.java
+++ Main.java
@@ -4,9 +4,9 @@ import java.util.stream.Collectors;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        List<Integer> nums = Arrays.stream(sc.nextLine().split(" ")).map(Integer::parseInt).collect(Collectors.toList());
-
-        System.out.println(nums.getFirst().equals(nums.getLast()));
+        List<Integer> nums1 = Arrays.stream(sc.nextLine().split(" ")).map(Integer::parseInt).collect(Collectors.toList());
+        List<Integer> nums2 = Arrays.stream(sc.nextLine().split(" ")).map(Integer::parseInt).collect(Collectors.toList());
+        System.out.println(nums1.getFirst().equals(nums2.getFirst()) || nums1.getLast().equals(nums2.getLast()));
 
     }
 }
