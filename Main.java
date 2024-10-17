fdf420121357f2ca6b8a4191f1bc6b3f0e085553
V-Gulev <velizargulev@abv.bg>
2024-10-17 18:31:33 +0300

    Exercise 96

--- Main.java
+++ Main.java
@@ -4,11 +4,18 @@ import java.util.stream.Collectors;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = Integer.parseInt(sc.nextLine());
-        String[] array = new String[n];
-        for (int i = 0; i < n; i++) {
-            array[i] = String.valueOf(i);
+        List<Integer> numbers = Arrays.stream(sc.nextLine().split(" ")).map(Integer::parseInt).collect(Collectors.toList());
+        boolean checkIfThereIs20 = false;
+        if (numbers.contains(10)) {
+            int indexOf10 = numbers.indexOf(10);
+            for (int i = indexOf10; i < numbers.size(); i++) {
+                if (numbers.get(i) == 20) {
+                    checkIfThereIs20 = true;
+                    break;
+                }
+            }
         }
-        System.out.println(Arrays.toString(array));
+        System.out.println(checkIfThereIs20);
+
     }
 }
