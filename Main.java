20f07fd6bd7decad533cd5e2dc163daf5f769d8d
V-Gulev <velizargulev@abv.bg>
2024-10-17 15:53:17 +0300

    Exercise 83

--- Main.java
+++ Main.java
@@ -5,7 +5,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         List<Integer> nums1 = Arrays.stream(sc.nextLine().split(" ")).map(Integer::parseInt).collect(Collectors.toList());
-        int[] array = {nums1.getLast() , nums1.get(1) , nums1.getFirst()};
-        System.out.println(Arrays.toString(array));
+        List<Integer> nums2 = Arrays.stream(sc.nextLine().split(" ")).map(Integer::parseInt).collect(Collectors.toList());
+        int[] result = new int[Math.min(nums2.size(), nums1.size())];
+        for (int i = 0; i < Math.min(nums2.size(), nums1.size()); i++) {
+            result[i] = nums1.get(i) * nums2.get(i);
+        }
+        System.out.println(Arrays.toString(result));
     }
 }
