b35fd61b4b637b6744e0f4b44307855ca7374de4
V-Gulev <velizargulev@abv.bg>
2024-10-17 15:28:29 +0300

    Exercise 74

--- Main.java
+++ Main.java
@@ -4,34 +4,9 @@ import java.util.stream.Collectors;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        List<Integer> numbers = Arrays.stream(sc.nextLine().split(" ")).map(Integer::parseInt).collect(Collectors.toList());
-        double average = 0;
-        int sum = 0;
-        for (int num : numbers) {
-            sum += num;
-        }
-        average = (double) sum / numbers.size();
-        List<Integer> biggerThanAverage = new ArrayList<>();
-        for (int num : numbers) {
-            if (num > average) {
-                biggerThanAverage.add(num);
-            }
-        }
-        biggerThanAverage.sort(Comparator.reverseOrder());
+        List<Integer> nums = Arrays.stream(sc.nextLine().split(" ")).map(Integer::parseInt).collect(Collectors.toList());
 
-        if (biggerThanAverage.size() > 5) {
-            int intsToRemove = biggerThanAverage.size() - 5;
-            for (int i = 0; i < intsToRemove; i++) {
-                biggerThanAverage.removeLast();
-            }
-        }
+        System.out.println(nums.getFirst() == 10 || nums.getLast() == 10);
 
-        if (biggerThanAverage.isEmpty()) {
-            System.out.println("No");
-        } else {
-            for (int num : biggerThanAverage) {
-                System.out.print(num + " ");
-            }
-        }
     }
 }
