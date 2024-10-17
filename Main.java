fc2a7b269fc16981e1c7a84455743d70e733d9a8
V-Gulev <velizargulev@abv.bg>
2024-10-17 17:42:04 +0300

    Exercise 91

--- Main.java
+++ Main.java
@@ -3,11 +3,13 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("PATH: ");
-        System.out.println(System.getenv("PATH"));
-        System.out.println("TEMP: ");
-        System.out.println(System.getenv("TEMP"));
-        System.out.println("USERNAME: ");
-        System.out.println(System.getenv("USERNAME"));
+        long startTime = System.nanoTime();
+        int i;
+        System.out.println("The first 10 numbers are:");
+        for (i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+        long estimatedTime = System.nanoTime() - startTime;
+        System.out.println("Estimated time in nanoseconds to get the first 10 natural numbers: " + estimatedTime);
     }
 }
