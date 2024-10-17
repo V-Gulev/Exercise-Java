99e3268b7789f5e505fa8ca24b9f232b13f4d0bf
V-Gulev <velizargulev@abv.bg>
2024-10-17 15:48:26 +0300

    Exercise 81

--- Main.java
+++ Main.java
@@ -5,8 +5,8 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         List<Integer> nums1 = Arrays.stream(sc.nextLine().split(" ")).map(Integer::parseInt).collect(Collectors.toList());
-        System.out.println(Math.max(nums1.getFirst(), nums1.getLast()));
-
-
+        int firstLast = Math.max(nums1.getFirst(), nums1.getLast());
+        int max = Math.max(firstLast, nums1.get(nums1.size() / 2));
+        System.out.println(max);
     }
 }
