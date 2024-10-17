4c159beb5239288819c87f7c4e71d09188d10806
V-Gulev <velizargulev@abv.bg>
2024-10-17 15:55:55 +0300

    Exercise 84

--- Main.java
+++ Main.java
@@ -4,12 +4,8 @@ import java.util.stream.Collectors;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        List<Integer> nums1 = Arrays.stream(sc.nextLine().split(" ")).map(Integer::parseInt).collect(Collectors.toList());
-        List<Integer> nums2 = Arrays.stream(sc.nextLine().split(" ")).map(Integer::parseInt).collect(Collectors.toList());
-        int[] result = new int[Math.min(nums2.size(), nums1.size())];
-        for (int i = 0; i < Math.min(nums2.size(), nums1.size()); i++) {
-            result[i] = nums1.get(i) * nums2.get(i);
-        }
-        System.out.println(Arrays.toString(result));
+        String input = sc.nextLine();
+        String word = input.substring(input.length() - 3);
+        System.out.println(word + input + word);
     }
 }
