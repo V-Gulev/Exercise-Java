6af8504294bb1917ff84d31a96d5c4eac8c4f055
V-Gulev <velizargulev@abv.bg>
2024-10-17 17:22:26 +0300

    Exercise 89

--- Main.java
+++ Main.java
@@ -3,9 +3,12 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Current system environment:");
-        System.out.println(System.getenv());
-        System.out.println("Current system properties:");
-        System.out.println(System.getProperties());
+        SecurityManager securityManager = System.getSecurityManager();
+        
+        if (securityManager != null) {
+            System.out.println("A security manager is already established.");
+        } else {
+            System.out.println("No security manager is established.");
+        }
     }
 }
