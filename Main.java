6e3e8b75efa76ef518889b66b71e4a96031a1183
V-Gulev <velizargulev@abv.bg>
2024-10-18 14:58:32 +0300

    Exercise 98

--- Main.java
+++ Main.java
@@ -5,19 +5,22 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         List<Integer> numbers = Arrays.stream(sc.nextLine().split(" ")).map(Integer::parseInt).collect(Collectors.toList());
-        int specifiedNumber = Integer.parseInt(sc.nextLine());
-        boolean twoNumbersTogether = false;
-        if (numbers.contains(specifiedNumber)) {
-            for (int i = numbers.indexOf(specifiedNumber); i < numbers.size() - 2; i++) {
-                if (numbers.get(i) == specifiedNumber && numbers.get(i + 1) == specifiedNumber) {
-                    twoNumbersTogether = true;
-                    break;
-                } else if (numbers.get(i) == specifiedNumber && i <= numbers.size() - 3 && numbers.get(i + 2) == specifiedNumber) {
-                    twoNumbersTogether = true;
+        boolean together20 = false;
+        int count20 = 0;
+        if (numbers.contains(20)) {
+            for (int i = 0; i < numbers.size() - 1; i++) {
+                if (numbers.get(i) == 20) {
+                    count20++;
+                }
+                if (numbers.get(i + 1) == 20 && numbers.get(i) == 20) {
+                    together20 = true;
                     break;
                 }
+
             }
         }
-        System.out.println(twoNumbersTogether);
+        if (numbers.getLast() == 20) count20++;
+        System.out.println(count20 == 3 && !together20);
+
     }
 }
