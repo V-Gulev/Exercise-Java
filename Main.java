b6366cdcc6878c4216ca2159094d01d037110010
V-Gulev <velizargulev@abv.bg>
2024-10-18 15:22:08 +0300

    Exercise 102

--- Main.java
+++ Main.java
@@ -5,15 +5,6 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         List<Integer> list1 = Arrays.stream(sc.nextLine().split(" ")).map(Integer::parseInt).collect(Collectors.toList());
-        int count10 = 0;
-        int count20 = 0;
-        for (int i = 0; i < list1.size(); i++) {
-            if (list1.get(i) == 10) {
-                count10++;
-            } else if (list1.get(i) == 20) {
-                count20++;
-            }
-        }
-        System.out.println(count10 > count20);
+        System.out.println(list1.contains(10) || list1.contains(30));
     }
 }
