67635d730cb55a727d70fd95ee5db7bcfba07fc3
V-Gulev <velizargulev@abv.bg>
2024-10-18 15:31:46 +0300

    Exercise 105

--- Main.java
+++ Main.java
@@ -5,14 +5,13 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         List<Integer> list1 = Arrays.stream(sc.nextLine().split(", ")).map(Integer::parseInt).collect(Collectors.toList());
-        List<Integer> result = new ArrayList<>();
+        int groupAmount = Integer.parseInt(sc.nextLine());
+        List<Integer> leftGroup = new ArrayList<>();
+        List<Integer> rightGroup = new ArrayList<>();
 
-        if (list1.contains(10)) {
-            int index = list1.lastIndexOf(10);
-            for (int i = 0; i < index; i++) {
-                result.add(list1.get(i));
-            }
-        }
-        System.out.println(result);
+        leftGroup = list1.subList(0, groupAmount);
+        rightGroup = list1.subList(list1.size() - groupAmount, list1.size());
+
+        System.out.println(leftGroup.equals(rightGroup));
     }
 }
