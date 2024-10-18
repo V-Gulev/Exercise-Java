f9e5987efa63b75fad236872f0fe2b96687149a7
V-Gulev <velizargulev@abv.bg>
2024-10-18 15:39:42 +0300

    Exercise 107

--- Main.java
+++ Main.java
@@ -5,9 +5,14 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         List<Integer> list1 = Arrays.stream(sc.nextLine().split(", ")).map(Integer::parseInt).collect(Collectors.toList());
-        int temp = list1.getFirst();
-        list1.removeFirst();
-        list1.add(temp);
-        System.out.println(list1);
+        boolean consecutiveNumbers = false;
+
+        for (int i = 0; i < list1.size() - 3; i++) {
+            if (list1.get(i + 1) == list1.get(i) + 1 && list1.get(i + 2) == list1.get(i + 1) + 1 && list1.get(i + 3) == list1.get(i + 2) + 1) {
+                consecutiveNumbers = true;
+                break;
+            }
+        }
+        System.out.println(consecutiveNumbers);
     }
 }
