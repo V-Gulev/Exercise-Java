9b4326a272d804b32e96af83ae6ad5e6c78f018c
V-Gulev <velizargulev@abv.bg>
2024-10-18 14:40:56 +0300

    Exercise 97

--- Main.java
+++ Main.java
@@ -5,17 +5,19 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         List<Integer> numbers = Arrays.stream(sc.nextLine().split(" ")).map(Integer::parseInt).collect(Collectors.toList());
-        boolean checkIfThereIs20 = false;
-        if (numbers.contains(10)) {
-            int indexOf10 = numbers.indexOf(10);
-            for (int i = indexOf10; i < numbers.size(); i++) {
-                if (numbers.get(i) == 20) {
-                    checkIfThereIs20 = true;
+        int specifiedNumber = Integer.parseInt(sc.nextLine());
+        boolean twoNumbersTogether = false;
+        if (numbers.contains(specifiedNumber)) {
+            for (int i = numbers.indexOf(specifiedNumber); i < numbers.size() - 2; i++) {
+                if (numbers.get(i) == specifiedNumber && numbers.get(i + 1) == specifiedNumber) {
+                    twoNumbersTogether = true;
+                    break;
+                } else if (numbers.get(i) == specifiedNumber && i <= numbers.size() - 3 && numbers.get(i + 2) == specifiedNumber) {
+                    twoNumbersTogether = true;
                     break;
                 }
             }
         }
-        System.out.println(checkIfThereIs20);
-
+        System.out.println(twoNumbersTogether);
     }
 }
