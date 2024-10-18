4a741a9c0a1ea17883cff4ee639f912176f1ae05
V-Gulev <velizargulev@abv.bg>
2024-10-18 15:25:10 +0300

    Exercise 103

--- Main.java
+++ Main.java
@@ -4,7 +4,15 @@ import java.util.stream.Collectors;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        List<Integer> list1 = Arrays.stream(sc.nextLine().split(" ")).map(Integer::parseInt).collect(Collectors.toList());
-        System.out.println(list1.contains(10) || list1.contains(30));
+        List<Integer> list1 = Arrays.stream(sc.nextLine().split(", ")).map(Integer::parseInt).collect(Collectors.toList());
+        List<Integer> result = new ArrayList<>();
+
+        if (list1.contains(10)) {
+            int index = list1.lastIndexOf(10);
+            for (int i = index + 1; i < list1.size(); i++) {
+                result.add(list1.get(i));
+            }
+        }
+        System.out.println(result);
     }
 }
