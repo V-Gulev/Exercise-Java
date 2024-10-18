5942c42fada24fdd9f339a79e9ea021127a25e57
V-Gulev <velizargulev@abv.bg>
2024-10-18 15:19:15 +0300

    Exercise 101

--- Main.java
+++ Main.java
@@ -5,15 +5,15 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         List<Integer> list1 = Arrays.stream(sc.nextLine().split(" ")).map(Integer::parseInt).collect(Collectors.toList());
-        List<Integer> list2 = Arrays.stream(sc.nextLine().split(" ")).map(Integer::parseInt).collect(Collectors.toList());
-        int counter = 0;
+        int count10 = 0;
+        int count20 = 0;
         for (int i = 0; i < list1.size(); i++) {
-            int diff = list1.get(i) - list2.get(i);
-            if (Math.abs(diff) <= 1 && !list1.get(i).equals(list2.get(i))) {
-                counter++;
+            if (list1.get(i) == 10) {
+                count10++;
+            } else if (list1.get(i) == 20) {
+                count20++;
             }
         }
-        System.out.println(counter);
-
+        System.out.println(count10 > count20);
     }
 }
