826321cd47dd644d0d791be9b64e0d0b98f42df1
V-Gulev <velizargulev@abv.bg>
2024-10-18 15:06:32 +0300

    Exercise 99

--- Main.java
+++ Main.java
@@ -5,22 +5,15 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         List<Integer> numbers = Arrays.stream(sc.nextLine().split(" ")).map(Integer::parseInt).collect(Collectors.toList());
-        boolean together20 = false;
-        int count20 = 0;
-        if (numbers.contains(20)) {
-            for (int i = 0; i < numbers.size() - 1; i++) {
-                if (numbers.get(i) == 20) {
-                    count20++;
-                }
-                if (numbers.get(i + 1) == 20 && numbers.get(i) == 20) {
-                    together20 = true;
-                    break;
-                }
+        boolean numAppearsInPairs = true;
+        int specifiedNumber = Integer.parseInt(sc.nextLine());
 
+        for (int i = 0; i < numbers.size() - 1; i++) {
+            if (numbers.get(i) != specifiedNumber && numbers.get(i + 1) != specifiedNumber) {
+                numAppearsInPairs = false;
+                break;
             }
         }
-        if (numbers.getLast() == 20) count20++;
-        System.out.println(count20 == 3 && !together20);
-
+        System.out.println(numAppearsInPairs);
     }
 }
