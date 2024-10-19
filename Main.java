1a0e5cce5c2c971f81d5e636ba62a8f042e81029
V-Gulev <velizargulev@abv.bg>
2024-10-19 16:14:25 +0300

    Exercise 111

--- Main.java
+++ Main.java
@@ -3,13 +3,15 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int number = Integer.parseInt(sc.nextLine());
-        int startingNumber = number;
-        while (number % 4 == 0) {
-            number /= 4;
+        System.out.print("Input first number: ");
+        int x = sc.nextInt();
+        System.out.print("Input second number: ");
+        int y = sc.nextInt();
+        while (y != 0) {
+            int carry = x & y;
+            x = x ^ y;
+            y = carry << 1;
         }
-        if (number == 1) {
-            System.out.println(startingNumber + " is a power of 4");
-        } else System.out.println(startingNumber + " is not a power of 4");
+        System.out.println("Sum: " + x);
     }
 }
