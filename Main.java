6ece93114a25c69168bba78fddae231f32823ec1
V-Gulev <velizargulev@abv.bg>
2024-10-19 17:44:40 +0300

    Exercise 115

--- Main.java
+++ Main.java
@@ -1,19 +1,16 @@
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String input = sc.nextLine();
-        int offset = sc.nextInt();
-        int length = input.length();
-        offset = offset % length;
-        String rotated = "";
-        for (int i = length - offset; i < length; i++) {
-            rotated += input.charAt(i);
+        String numberString = sc.nextLine();
+        char[] array = numberString.toCharArray();
+        String palindrome = "";
+        for (int i = array.length - 1; i >= 0; i--) {
+            palindrome += array[i];
         }
-        for (int i = 0; i < length - offset; i++) {
-            rotated += input.charAt(i);
-        }
-        System.out.println("Rotated string: " + rotated);
+        System.out.println(palindrome.equals(numberString));
+
     }
 }
