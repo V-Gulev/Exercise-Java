2935aae99f55cccdebf43c325daef44e4d1e66e1
V-Gulev <velizargulev@abv.bg>
2024-10-19 18:04:36 +0300

    Exercise 120

--- Main.java
+++ Main.java
@@ -4,10 +4,25 @@ import java.util.stream.Collectors;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String string = sc.nextLine();
-        String target = sc.nextLine();
-        if (string.contains(target)) {
-            System.out.println(string.lastIndexOf(target) + target.length() - 1);
+        int[][] matrix = new int[3][3];
+        System.out.println("Enter the values:");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
         }
+        System.out.print("Enter what you search for: ");
+        int target = sc.nextInt();
+        boolean found = false;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (matrix[i][j] == target) {
+                    found = true;
+                    break;
+                }
+            }
+            if (found) break;
+        }
+        System.out.println(found);
     }
 }
