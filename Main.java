a475f25f59ee4daf98a3da7983b6fc388d48e84b
V-Gulev <velizargulev@abv.bg>
2024-10-19 17:50:46 +0300

    Exercise 117

--- Main.java
+++ Main.java
@@ -4,15 +4,8 @@ import java.util.stream.Collectors;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        for (int i = 1; i <= 100; i++) {
-            if (i % 3 == 0 && i % 5 == 0) {
-                System.out.println("fizz buzz");
-            } else if (i % 5 == 0) {
-                System.out.println("Buzz");
-            } else if (i % 3 == 0) {
-                System.out.println("Fizz");
-            }
-        }
-
+        System.out.print("Input a number: ");
+        int num = sc.nextInt();
+        System.out.println(Math.sqrt(num));
     }
 }
