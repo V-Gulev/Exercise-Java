2999e929ba84e0020d5d113049078c34dd83879f
V-Gulev <velizargulev@abv.bg>
2024-10-19 17:58:35 +0300

    Exercise 118

--- Main.java
+++ Main.java
@@ -4,8 +4,10 @@ import java.util.stream.Collectors;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Input a number: ");
-        int num = sc.nextInt();
-        System.out.println(Math.sqrt(num));
+        String string = sc.nextLine();
+        String target = sc.nextLine();
+        if (string.contains(target)) {
+            System.out.println(string.indexOf(target) + target.length() - 1);
+        }
     }
 }
