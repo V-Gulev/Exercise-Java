f6cb0d49aa30a3137e2ad1d5db9f061d461f1781
V-Gulev <velizargulev@abv.bg>
2024-10-20 18:31:49 +0300

    Exercise 151

--- Main.java
+++ Main.java
@@ -1,52 +1,10 @@
-class LinkedList {
-
-    static Node head;
-
-    static class Node {
-        int data;
-        Node next_node;
-
-        Node(int d) {
-            data = d;
-            next_node = null;
-        }
-    }
-
-    Node reverse(Node node) {
-        Node previousNode = null;
-        Node currentNode = node;
-        Node nextNode = null;
-        while (currentNode != null) {
-            nextNode = currentNode.next_node;
-            currentNode.next_node = previousNode;
-            previousNode = currentNode;
-            currentNode = nextNode;
-        }
-        node = previousNode;
-        return node;
-    }
-
-    void printList(Node node) {
-        while (node != null) {
-            System.out.print(node.data + " ");
-            node = node.next_node;
-        }
-    }
-
+public class Main {
     public static void main(String[] args) {
-        LinkedList list = new LinkedList();
-        list.head = new Node(20);
-        list.head.next_node = new Node(40);
-        list.head.next_node.next_node = new Node(60);
-        list.head.next_node.next_node.next_node = new Node(80);
-
-        System.out.println("Original Linked list:");
-        list.printList(head);
-
-        head = list.reverse(head);
-
-        System.out.println("");
-        System.out.println("Reversed Linked list:");
-        list.printList(head);
+        System.out.println((101 + 0) / 3);
+        System.out.println(3.0e-6 * 10000000.1);
+        System.out.println(true && true);
+        System.out.println(false && true);
+        System.out.println((false && false) || (true && true));
+        System.out.println((false || false) && (true && true));
     }
 }
