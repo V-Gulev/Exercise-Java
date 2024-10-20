91537ef4aaf2ad97890e2db05ae4440123a8eb75
V-Gulev <velizargulev@abv.bg>
2024-10-20 18:46:06 +0300

    Exercise 159

--- Main.java
+++ Main.java
@@ -1,11 +1,17 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        double a = sc.nextDouble();
-        double b = sc.nextDouble();
-        System.out.println(a < 1 && a > 0 && b < 1 && b > 0);
-
+        List<Integer> numbers = Arrays.stream(sc.nextLine().split(" ")).map(Integer::parseInt).collect(Collectors.toList());
+        int amount = sc.nextInt();
+        numbers.sort(Comparator.reverseOrder());
+        for (int i = 0; i < amount; i++) {
+            System.out.print(numbers.get(i) + " ");
+        }
     }
 }
