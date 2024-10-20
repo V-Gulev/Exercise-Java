50b22f903a669b11ded29c109f71cdda229d2fb5
V-Gulev <velizargulev@abv.bg>
2024-10-20 18:37:10 +0300

    Exercise 153

--- Main.java
+++ Main.java
@@ -3,12 +3,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = sc.nextInt();
-        int d = sc.nextInt();
-        if (a == b && a == c & a == d) {
-            System.out.println("Numbers are equal");
-        } else System.out.println("Numbers are not equal");
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        System.out.println(a < 1 && a > 0 && b < 1 && b > 0);
+
     }
 }
