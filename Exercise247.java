346b8900b88c977ca4deeeeecdeae2eb68163f7d
V-Gulev <velizargulev@abv.bg>
2025-01-04 20:30:10 +0200

    Exercise247

---
+++ Exercise247.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Exercise247 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Input the numbers: a, b, c");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        System.out.print("Check if a + b is greater than c: ");
+        System.out.println(a + b > c);
+
+
+
+
+    }
+}
