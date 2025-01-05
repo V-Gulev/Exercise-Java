c915f135f10fc511a8c69b312fafddd2b852a5cd
V-Gulev <velizargulev@abv.bg>
2025-01-05 20:47:09 +0200

    Exercise249

---
+++ Exercise249.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Exercise249 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input a number: ");
+        String input = sc.nextLine();
+        int counter = 0;
+        for (char ch : input.toCharArray()) {
+            if (ch != '0') {
+                counter++;
+            }
+        }
+        System.out.println("Hamming weight: " + counter);
+
+    }
+}
