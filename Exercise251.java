c3e71135c2784d6ef0bd41fa31261175f28b4db9
V-Gulev <velizargulev@abv.bg>
2025-01-06 20:13:49 +0200

    Exercise251

---
+++ Exercise251.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Exercise251 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input your three numbers: ");
+        double n1 = sc.nextDouble();
+        double n2 = sc.nextDouble();
+        double n3 = sc.nextDouble();
+        System.out.println("The average is: " + average(n1, n2, n3));
+    }
+
+    public static double average(double n1, double n2, double n3) {
+        return (n1 + n2 + n3) / 3;
+    }
+}
