d5cff062ddd26f98100b1ad8a6988a3e4a8e11e4
V-Gulev <velizargulev@abv.bg>
2025-01-06 19:49:36 +0200

    Exercise250

---
+++ Exercise250.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Exercise250 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input your three numbers: ");
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
+        int n3 = sc.nextInt();
+        System.out.println("The smallest number is: " + smallestNumber(n1, n2, n3));
+    }
+
+    public static int smallestNumber(int n1, int n2, int n3) {
+        int n4 = Math.min(n1, n2);
+        return Math.min(n4, n3);
+    }
+}
