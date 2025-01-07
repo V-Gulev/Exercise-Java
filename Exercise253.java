dcd83b77d1e0fc4e8b226eb924d4bf63fea83ccc
V-Gulev <velizargulev@abv.bg>
2025-01-07 18:02:14 +0200

    Exercise253

---
+++ Exercise253.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Exercise253 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input your text: ");
+        String text = sc.nextLine();
+        System.out.println("The number of vowels is: " + countVowels(text));
+
+    }
+
+    public static int countVowels(String input) {
+        int counter = 0;
+        if (!input.isEmpty()) {
+            for (int i = 0; i < input.length(); i++) {
+                if (input.charAt(i) == 'a' || input.charAt(i) == 'o' || input.charAt(i) == 'e' || input.charAt(i) == 'i' || input.charAt(i) == 'u') {
+                    counter++;
+                }
+            }
+        }
+        return counter;
+    }
+}
