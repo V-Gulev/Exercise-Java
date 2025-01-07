910fa5440af5ebd830dd6a227347a80d3dea7f7f
V-Gulev <velizargulev@abv.bg>
2025-01-07 18:10:32 +0200

    Exercise254

---
+++ Exercise254.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Exercise254 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input your text: ");
+        String[] text = sc.nextLine().replaceAll(",", " ").replaceAll("\\.", " ")
+                .replaceAll("!", " ").replaceAll("\\?", " ")
+                .split("\\s+");
+
+        System.out.println("The amount of words on the text is: " + getWordsAmount(text));
+
+
+    }
+
+    public static int getWordsAmount(String[] text) {
+        return text.length;
+    }
+}
