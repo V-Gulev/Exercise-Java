42ccd255e222cf31a411a8641e41e0fae067699e
V-Gulev <velizargulev@abv.bg>
2025-05-01 18:57:50 +0300

    Exercise274

---
+++ Exercises/Exercise274.java
@@ -0,0 +1,19 @@
+package Exercises;
+
+public class Exercise274 {
+    public enum Shape {
+        SQUARE,
+        CIRCLE,
+        RECTANGLE,
+        TRIANGLE,
+        HEXAGON
+    }
+
+    public static void main(String[] args) {
+        Shape shape1 = Shape.CIRCLE;
+        Shape shape2 = Shape.SQUARE;
+
+        System.out.println("First shape: " + shape1);
+        System.out.println("Second shape: " + shape2);
+    }
+}
